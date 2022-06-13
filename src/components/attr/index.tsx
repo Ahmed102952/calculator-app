9c415b7e1c6e511446045fac097487fae68957a8
Ahmed102952 <ahmeda8400@gmail.com>
2022-06-14 01:13:35 +0200

    😍👌

---
+++ src/components/attr/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Attr = () => {
+  return (
+    <div className="attribution w-fit absolute left-14 top-6 text-opacity-60 drop-shadow-lg shadow-blue-100 -translate-x-1/2 text-base text-black font-bold">
+      <a href="https://github.com/Ahmed102952" target="_blank" rel="noreferrer">
+        Angel
+      </a>
+    </div>
+  );
+};
+
+export default Attr;
